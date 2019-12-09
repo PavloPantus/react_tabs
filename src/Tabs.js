a07205acdd67c8f91e8a67cf0a140294893a488a
PavloPantus <mr.pantus@gmail.com>
2019-12-09 22:12:06 +0200

    corected props validation, uses classnames lib

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const classNames = require('classnames');
+
 class Tabs extends React.Component {
-  state = { i: 0 };
+  state = { currentIndex: 0 };
 
   render() {
-    const { i } = this.state;
+    const { currentIndex } = this.state;
 
     return (
       <>
         <ul className="App__listOfTabs">
           {this.props.tabs.map(
-            (tab, index) => (
-              <li key={tab.title}>
-                <button
-                  type="button"
-                  key={tab.title}
-                  onClick={() => {
-                    this.setState({ i: index });
-                  }}
-                  className={
-                    `
-                  App__tab ${this.state.i === index
-                      ? 'App__tab_active' : ''}
-                  `
-                  }
-                >
-                  {tab.title}
-                </button>
-              </li>
-            )
+            (tab, index) => {
+              const btnClass = classNames(
+                'App__tab',
+                { App__tab_active: this.state.currentIndex === index }
+              );
+
+              return (
+                <li key={tab.title}>
+                  <button
+                    type="button"
+                    key={tab.title}
+                    onClick={() => {
+                      this.setState({ currentIndex: index });
+                    }}
+
+                    className={btnClass}
+                  >
+                    {tab.title}
+                  </button>
+                </li>
+              );
+            }
           )
           }
         </ul>
         <h2 className="App__tabContent">
-          {this.props.tabs[i].content}
+          {this.props.tabs[currentIndex].content}
         </h2>
       </>
     );
   }
 }
 
-Tabs.propTypes = { tabs: PropTypes.arrayOf(PropTypes.any) };
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.shape(
+    {
+      title: PropTypes.string,
+      content: PropTypes.string,
+    }
+  )),
+};
 
 Tabs.defaultProps = { tabs: [] };
 
